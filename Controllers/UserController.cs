21381d1b8e04a77d2caed55bff67cea40df1ce08
chrisdanbg <chrisdanbg@icloud.com>
2019-02-25 19:33:17 +0200

    Added Authenticate in UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,19 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Auth.Dtos;
+using Auth.Helpers;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Auth.Controllers
 {
-    public class UserController : Controller
+    [Authorize, ApiController, Route("[controller]")]
+    public class UserController : ControllerBase
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private IUserService _repo;
+        private IMapper _mapper;
+        private AppSettings _appSettings;
+
+        public UserController(IUserService repo, IMapper mapper, AppSettings appSettings)
         {
-            return View();
+            _repo = repo;
+            _mapper = mapper;
+            _appSettings = appSettings;
+        }
+
+        public SigningCredentials SigningCredentials { get; private set; }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]UserForCreationDto userForCreationDto)
+        {
+            var user = await _repo.Authenticate(userForCreationDto.Username, userForCreationDto.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.Username,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = token
+            });
         }
     }
+
+
 }
---
+++ Helpers/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Auth.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
